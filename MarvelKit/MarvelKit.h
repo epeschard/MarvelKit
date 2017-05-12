cc8fa11570583e82a4ec35cf1b7ba031b076c4ad
Eugene Peschard <e@peschard.me>
2017-05-12 11:45:24 +0200

    Initial Commit

---
+++ MarvelKit/MarvelKit.h
@@ -0,0 +1,19 @@
+//
+//  MarvelKit.h
+//  MarvelKit
+//
+//  Created by Eugène Peschard on 12/05/2017.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MarvelKit.
+FOUNDATION_EXPORT double MarvelKitVersionNumber;
+
+//! Project version string for MarvelKit.
+FOUNDATION_EXPORT const unsigned char MarvelKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MarvelKit/PublicHeader.h>
+
+
